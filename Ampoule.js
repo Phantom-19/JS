75bdfc26a73777e3294b5bb48bf17d7c6791d96f
Faxel <63906142+Phantom-19@users.noreply.github.com>
2020-09-25 19:28:52 +0000

    Create Ampoule.js

---
+++ Ampoule.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+ <body>
+  <h2>Ampoule en JavaScript Do?</h2>
+   <p>Une fonction d'allumage de l'ampoule et l'éteindre</p>
+    <button onclick="document.getElementById('monImage').src='ampoule.gif'">Allumer l'ampoule</button>
+     <img id="monImage" src="ampoule.gif" style="width:100px">
+    <button onclick="document.getElementById('monImage').src='ampoule.gif'">Eteindre l'ampoule</button>
+ </body>
+</html>
