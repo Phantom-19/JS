98b6d4955a1de513fd897825c80856c400aa0c11
Faxel <63906142+Phantom-19@users.noreply.github.com>
2020-09-25 19:43:09 +0000

    Update Ampoule.js

--- Ampoule.js
+++ Ampoule.js
@@ -3,8 +3,8 @@
  <body>
   <h2> Ampoule en JavaScript?</h2>
    <p> Une fonction d'allumage de l'ampoule et l'éteindre </p>
-    <button onclick="document.getElementById('monImage').src='https://github.com/Phantom-19/JS/blob/master/ampoule.gif'">Allumer l'ampoule</button>
-     <img id="monImage" src="https://github.com/Phantom-19/JS/blob/master/ampoule.gif" style="width:100px">
-    <button onclick="document.getElementById('monImage').src='https://github.com/Phantom-19/JS/blob/master/ampoule.gif'">Eteindre l'ampoule</button>
+    <button onclick="document.getElementById('monImage').src='pic_bulboff.gif'">Allumer l'ampoule</button>
+     <img id="monImage" src="pic_bulboff.gif" style="width:100px">
+    <button onclick="document.getElementById('monImage').src='pic_bulboff.gif'">Eteindre l'ampoule</button>
  </body>
 </html>
